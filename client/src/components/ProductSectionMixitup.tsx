a7ef5100d079afa7b7d6483583b87df717d62968
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2022-12-15 14:21:23 +0700

    Mixitup

--- client/src/components/ProductSectionMixitup.tsx
+++ client/src/components/ProductSectionMixitup.tsx
@@ -4,21 +4,23 @@ import PagePreloder from "./PagePreloder"
 import { useDispatch } from 'react-redux'
 import { addToCart } from 'reducers/productsReducer'
 import ProductItem from './utilities/ProductItem'
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { request } from 'utilities/axios-utils'
 import mixitup from 'mixitup';
+import { ErrorBoundary } from 'react-error-boundary'
+import { ErrorFallback, myErrorHandler } from 'utilities/error-boundary-utils'
 
 const ProductSectionMixitup = () => {
     const dispatch = useDispatch()
-
+    const [productTypes, setProductType] = useState([])
     const handleAddToCart = useCallback((productId: number) => {
         dispatch(addToCart({ id: productId, quantity: 1 }));
     }, [])
     const queryClient = useQueryClient()
-    const { isLoading, data, error } = useQuery(
+    const { isLoading, data, error, isSuccess } = useQuery(
         'products',
         () => {
-            return request({ url: '/products?_expand=productType&productTypeId_ne=0&_limit=8' })
+            return request({ url: '/products?_expand=productType&productTypeId_ne=0&_limit=8/productTypes' })
         },
         {
             select: (data) => {
@@ -28,8 +30,24 @@ const ProductSectionMixitup = () => {
                 return newProducts
             }
             ,
-            onSuccess: () => {
-                console.log("onSuccess")
+            onSuccess: (data) => {
+                const uniqueIds: any[] = [];
+
+                const unique = data?.filter((element: any) => {
+                    const isDuplicate = uniqueIds.includes(element.productType.type);
+
+                    if (!isDuplicate) {
+                        uniqueIds.push(element.productType.type);
+
+                        return element.productType.type;
+                    }
+
+                    return false;
+                });
+
+                setProductType(() => {
+                    return unique.map((type: any) => type.productType.type)
+                })
             },
             onError: (_error) => {
                 console.log(_error)
@@ -42,181 +60,66 @@ const ProductSectionMixitup = () => {
         }
     )
 
-    if (isLoading) return <>
-        <PagePreloder />
-    </>
-
     const handleAddWishlist = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault()
         console.log("add to wishlist")
     }
 
-    // The `Api` class is not part of MixItUp and has been created to mock
-    // the asyncronous fetching of data for this demo. To use it, we must
-    // create an api instance, passing in our mock data to represent the
-    // contents of a DB.
+    let mixer: any;
 
-    //var api = new Api(items);
+    useEffect(() => {
+        const myTimeout = setTimeout(() => {
+            const containerEl = document.querySelector('.product__filter');
+            mixer = mixitup(containerEl)
+            const firstFilterSelector =  document.querySelector('.filter__controls li') as HTMLElement | null;
+            mixer.filter(firstFilterSelector?.dataset.filter)
+        }, 100);
+        return (() => {
+            clearTimeout(myTimeout);
+        })
+    }, [isLoading, productTypes])
 
-    // As we'll be building and binding our own UI, we'll start with caching
-    // references to any DOM elements we'll need to work with
 
-    var controls = document.querySelector('[data-ref="controls"]');
-    var filters = document.querySelectorAll('[data-ref="filter"]');
-    var container = document.querySelector('[data-ref="container"]');
 
-    // "Gap" elements are used to maintain even columns in a justified grid. See our
-    // "MixItUp Grid Layouts" tutorial for more information.
-
-    var firstGap = document.querySelector('[data-ref="first-gap"]');
-
-    // We'll need to keep track of our active current filter so
-    // that we can sort within the current filter.
-
-    var activeColor = '';
-
-    // Instantiate and configure the mixer
-
-    /* var mixer = mixitup(container, {
-        selectors: {
-            target: '[data-ref="item"]' // Query targets with an attribute selector to keep our JS and styling classes seperate
-        },
-        layout: {
-            siblingAfter: firstGap // Ensure the first "gap" element is known to mixitup incase of insertion into an empty container
-        },
-        load: {
-            dataset: data // As we have pre-rendered targets, we must "prime" MixItUp with their underlying data
-        },
-        data: {
-            uidKey: 'id' // Our data model must have a unique id. In this case, its key is 'id'
-        },
-        render: { // We must provide a target render function incase we need to render new items not in the initial dataset (not used in this demo)
-            target: function (item:any) {
-                return '<div class="item ' + item.color + '" data-ref="item"><time class="published-date">' + item.publishedDate + '</time></div>';
-            }
-        }
-    }); */
-
-    /**
-     * A helper function to set an active styling class on an active button,
-     * and remove it from its siblings at the same time.
-     *
-     * @param {HTMLElement} activeButton
-     * @param {HTMLELement[]} siblings
-     * @return {void}
-     */
-
-    function activateButton(activeButton: any, siblings: any) {
-        var button;
-        var i;
-console.log(activeButton)
-        for (i = 0; i < siblings.length; i++) {
-            button = siblings[i];
-
-            button.classList[button === activeButton ? 'add' : 'remove']('control-active');
-        }
-    }
-
-    /**
-     * A click handler to detect the type of button clicked, read off the
-     * relevent attributes, call the API, and trigger a dataset operation.
-     *
-     * @param   {HTMLElement} button
-     * @return  {void}
-     */
-
-    function handleButtonClick(button: any) {
-        // Define default values for color, sortBy and order
-        // incase they are not present in the clicked button
-
-        var color = activeColor;
-        var sortBy = 'id';
-        var order = 'asc';
-
-        // If button is already active, or an operation is in progress, ignore the click
-
-        // if (button.classList.contains('control-active') || mixer.isMixing()) return;
-
-        // Else, check what type of button it is, if any
-
-        if (button.matches('[data-ref="filter"]')) {
-            // Filter button
-//console.log(button)
-            activateButton(button, filters);
-
-            color = activeColor = button.getAttribute('data-color');
-        } else {
-            // Not a button
-
-            return;
-        }
-
-        // Now that we have our color filter and sorting order, we can fetch some data from the API.
-
-        /*   api.get({
-              color: color,
-              $sort_by: sortBy,
-              $order: order
-          })
-              .then(function (items) {
-                  // Our api returns an array of items which we can send
-                  // straight to our mixer using the .dataset() method
-  
-                  return mixer.dataset(items);
-              })
-              .then(function (state) {
-                  console.log('fetched ' + state.activeDataset.length + ' items');
-              })
-              .catch(console.error.bind(console)); */
-    }
-
-    // We can now set up a handler to listen for "click" events on our UI buttons
-
-    controls?.addEventListener('click', function (e) {
-        handleButtonClick(e.target);
-        e.preventDefault()
-    });
-
-    // Set controls the active controls on startup to match the default filter and sort
-
-    activateButton(controls?.querySelector('[data-color="all"]'), filters);
-
-    // NB: Always remember to destroy the instance with mixer.destroy() when your
-    // component unmounts to ensure that event handlers are unbound and the
-    // instance can be garbage collected.
-
+    if (isLoading) return <>
+        <PagePreloder />
+    </>
     return (
         <>
             {/* Product Section Begin */}
             <section className="product spad">
-                <div className="container">
+                <div className="container" data-ref="container">
                     <div className="row">
                         <div className="col-lg-12">
-                            <ul className="filter__controls" data-ref="controls">
-                                <li className="active" data-filter="*" >
-                                    <button type="button"
-                                        className="control control-filter" data-ref="filter" data-color="all">Best Sellers</button>
-                                </li>
-                                <li data-filter=".new-arrivals">
-                                    <button type="button"
-                                        className="control control-filter" data-ref="filter" data-color="all">New Arrivals</button></li>
-                                <li data-filter=".hot-sales"><button type="button"
-                                    className="control control-filter" data-ref="filter" data-color="all">Hot Sales</button></li>
-                            </ul>
+                            {productTypes &&
+                                <ul className="filter__controls" data-ref="controls">
+                                    {productTypes.map((type:string) => {
+                                        return <li data-filter={`.${type.replace(' ', '-')}`} data-ref="filter">{type}</li>
+                                    })}
+                                </ul>
+                            }
                         </div>
                     </div>
-                    <div className="row product__filter" data-ref="container">
-                        {data?.map((product: any) => {
-                            return <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix">
+                    <ErrorBoundary FallbackComponent={ErrorFallback} onError={myErrorHandler}>
+                        <div className="row product__filter" ><>
+                            {data?.map((product: any) => {
+                                return <>
+                                    <div data-ref="item"
+                                        className={`col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix 
+                                     ${product.productType.type.replace(' ', '-')}`}>
 
-                                <ProductItem
-                                    key={"product-" + product.id}
-                                    product={product}
-                                    handleAddToCart={handleAddToCart}
-                                    handleAddWishlist={handleAddWishlist} /></div>
-                        })
-                        }
-                    </div>
+                                        <ProductItem
+                                            key={"product-" + product.id}
+                                            product={product}
+                                            handleAddToCart={handleAddToCart}
+                                            handleAddWishlist={handleAddWishlist} /></div>
+                                </>
+
+                            })
+                            }
+                        </>
+                        </div>
+                    </ErrorBoundary>
                 </div>
             </section>
             {/* Product Section End */}
